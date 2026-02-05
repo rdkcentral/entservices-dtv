33600dd47fa4eb9e5f1b6139c61c9138917f6380
smanes0213 <sankalpmaneshwar46@outlook.com>
2026-02-05 14:55:29 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -56,8 +56,6 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-dtv \
 -DUSE_DRM_SCREENCAPTURE -DHAS_API_SYSTEM -DHAS_API_POWERSTATE \
 -DHAS_RBUS -DDISABLE_SECURITY_TOKEN -DENABLE_DEVICE_MANUFACTURER_INFO -DUSE_THUNDER_R4 -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
 
-if [ "$PLUGIN_DTV" != "OFF" ]; then
-    cmake --build build/entservices-dtv --target install
-fi
+# cmake --build build/entservices-dtv --target install
 echo "======================================================================================"
 exit 0
