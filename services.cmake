f8211386f3cf7461f88b1c91c794dd905aa68db9
smanes0213 <sankalpmaneshwar46@outlook.com>
2026-02-05 12:33:49 +0530

    Clean service.cmake

--- services.cmake
+++ services.cmake
@@ -19,8 +19,6 @@
 # features
 #
 
-add_definitions (-DUSE_SOUND_PLAYER)
-
 add_definitions (-DUSE_IARM)
 option(USE_IARM "USE_IARM" ON)
 
@@ -32,7 +30,6 @@ add_definitions (-DUSE_IARMBUS)
 add_definitions (-DUSE_TR_69)
 
 add_definitions (-DHAS_API_SYSTEM)
-add_definitions (-DHAS_API_POWERSTATE)
 add_definitions(-DRDK_LOG_MILESTONE)
 
 add_definitions (-DUSE_DS)
