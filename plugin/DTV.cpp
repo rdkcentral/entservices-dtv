1e5acfa669936b135fce97d94af02fe0fff2b5bb
sreeja <sreeja_nampoothiri@comcast.com>
2026-03-10 11:55:31 +0530

    CMFSUPPORT-3993 : COVERITY TEST. DO NOT MERGE

--- plugin/DTV.cpp
+++ plugin/DTV.cpp
@@ -18,6 +18,7 @@
  */
 
 #include "DTV.h"
+#include <stdio.h>
 
 #define API_VERSION_NUMBER_MAJOR 1
 #define API_VERSION_NUMBER_MINOR 0
@@ -225,6 +226,7 @@ namespace WPEFramework
       uint32_t DTV::GetNumberOfCountries(Core::JSON::DecUInt8 &response) const
       {
          uint8_t count;
+         printf("Test coverity workflow %s");
          uint32_t result = m_dtv->GetNumberOfCountries(count);
          if (result == Core::ERROR_NONE)
          {
